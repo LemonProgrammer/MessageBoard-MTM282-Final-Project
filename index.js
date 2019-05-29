cc8ae4dc02998267820eecb40160d65fa40c8b7b
Fernando Blanco <fern.alej.blanco@gmail.com>
2019-05-29 15:12:18 -0600

    Revert "More changes"
    This reverts commit 2190394d2b4bb59e04e0db11631bcbb95461690e.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express'), 
+ pug = require('pug'), 
+ bodyParser = require('body-parser'),
+ route = require('./routes/routes.js'),
+ path = require('path');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname, + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+app.get('/', route.index);
+app.get('/viewProfile', route.viewProfile);
+app.get('/createAccount', route.createAccount);
+app.get('/editAccount', route.editAccount);
+app.get('/changeAvatar', route.changeAvatar);
+
+
+
+
+app.listen(8000);
