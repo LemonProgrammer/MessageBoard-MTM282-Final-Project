9963f1ff5cd5d18509b30b15958497730bf91c2e
Fernando Blanco <fern.alej.blanco@gmail.com>
2019-05-30 12:14:28 -0600

    Had to move some files around for mongodb

--- db.bat
+++ db.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath "data"
+"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath "C:\Users\Fernando\Documents\GitKraken\MessageBoardFinal\MessageBoard-MTM282-Final-Project\data"
