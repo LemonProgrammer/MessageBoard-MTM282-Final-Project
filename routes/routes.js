be126cccf0d5524de0acfa07313c112b62c1114d
Ernesto Cantero <ecantero@student.neumont.edu>
2019-05-31 11:59:35 -0600

    connection established
    Create the connect form mongoose to data folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,39 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-exports.index = (req,res) => {
-    res.render('home', {
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/data");
 
-    });
+const mdb = mongoose.connection;
+mdb.on("error", console.error.bind(console, "connection error:"));
+mdb.once("open", function(callback) {});
+
+const personSchema = mongoose.Schema({
+  //What would we added to this schema
+});
+
+const Person = mongoose.model("People_Collection", personSchema);
+
+exports.index = (req, res) => {
+  res.render("home", {});
 };
 
-exports.viewProfile = (req,res) => {
-
-    res.render('viewProfile', {
-        title: 'View Profile'
-    });
+exports.viewProfile = (req, res) => {
+  res.render("viewProfile", {
+    title: "View Profile"
+  });
 };
-exports.changeAvatar = (req,res) => {
-
-    res.render('changeAvatar', {
-        title: 'Change Avatar'
-    });
+exports.changeAvatar = (req, res) => {
+  res.render("changeAvatar", {
+    title: "Change Avatar"
+  });
 };
-exports.createAccount = (req,res) => {
-
-    res.render('createAccount', {
-        title: 'Create Account'
-    });
+exports.createAccount = (req, res) => {
+  res.render("createAccount", {
+    title: "Create Account"
+  });
 };
-exports.editAccount = (req,res) => {
-
-    res.render('editAccount', {
-        title: 'Edit Account'
-    });
+exports.editAccount = (req, res) => {
+  res.render("editAccount", {
+    title: "Edit Account"
+  });
 };
