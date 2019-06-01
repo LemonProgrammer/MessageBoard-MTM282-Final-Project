ef49f978c4b5d3929bfaafd62c07fc2939ca4113
Ernesto Cantero <ecantero@student.neumont.edu>
2019-05-31 19:14:28 -0600

    route

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/data");
+mongoose.connect("mongodb://localhost/data", { useNewUrlParser: true });
 
 const mdb = mongoose.connection;
 mdb.on("error", console.error.bind(console, "connection error:"));
