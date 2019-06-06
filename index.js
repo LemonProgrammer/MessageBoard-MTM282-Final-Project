477630b2de8fb14aab5d83eee28b368276c57a74
Fernando Blanco <fern.alej.blanco@gmail.com>
2019-06-06 03:16:53 -0600

    Still working on Edit Profile functionality

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.get('/home', route.index);
 app.get('/home/:id', route.index);
 app.get('/viewProfile/:id', route.viewProfile);
 app.get('/createAccount', route.createAccount);
-app.get('/editAccount/:id', route.editAccount);
 app.get('/changeAvatar', route.changeAvatar);
+app.get('/editAccount/:id', route.editAccount);
 app.get('/deleteAccount/:id', route.deleteAccount);
+app.post('/editAccount/:id', urlEncodedParser, route.makeChangesToAccount);
 app.post('/createAccount', urlEncodedParser, route.createNewAccount);
 app.post('/home', urlEncodedParser, route.verifyLogin);
-app.post('/editAccount/:id', route.updateProfileToDataBase);
 
 
 
